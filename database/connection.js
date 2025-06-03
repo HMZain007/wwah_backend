49d384144f6a651b63078eb952ec91271a0e2454
Umber Fatimi <159563995+UmberFatimi@users.noreply.github.com>
2025-06-04 01:58:43 +0500

    menay kr dikhayaaa

--- database/connection.js
+++ database/connection.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, {})
+mongoose.connect(uri, {});
 const db = mongoose.connection;
 // Connection successful
-db.on('connected', () => {
-    console.log('MongoDB connected with server  successfully');
+db.on("connected", () => {
+  console.log("MongoDB connected with server  successfully");
 });
 
 // Connection error
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
 });
 
 // Optional: Once the connection is open
-db.once('open', () => {
-    console.log('MongoDB connection is open');
+db.once("open", () => {
+  console.log("MongoDB connection is open");
 });
 
 // Optional: When the connection is disconnected
-db.on('disconnected', () => {
-    console.log('MongoDB disconnected');
+db.on("disconnected", () => {
+  console.log("MongoDB disconnected");
 });
