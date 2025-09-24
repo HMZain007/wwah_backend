3c053b6d6746268d326a04147ad3b83b154bcc35
Subhaina9 <100225283+Subhaina9@users.noreply.github.com>
2025-09-24 04:04:37 -0700

    sorting on admin side

--- routers/adminDashboard/studentData.js
+++ routers/adminDashboard/studentData.js
@@ -1,4 +1,43 @@
 
+// const express = require("express");
+// const UserDb = require("../../database/models/UserDb");
+// const applicationInfo = require("../../database/models/stdDashboard/applicationInfoDb");
+// const basicInfo = require("../../database/models/stdDashboard/basicInfoDb");
+// const userFiles = require("../../database/models/stdDashboard/uploadFilesDb");
+// const router = express.Router();
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const { id } = req.query; // Get user ID from query params
+
+//     if (id) {
+//       // Fetch a single user by ID
+//       const user = await UserDb.findOne({ _id: id, role: { $ne: "admin" } });
+
+//       if (!user) {
+//         return res.status(404).json({ message: "User not found" });
+//       }
+
+//       return res.json({ user }); // Return user object instead of array
+//     }
+
+//     // Fetch all users when no ID is provided
+//     const [Users, applications, basics, documents] = await Promise.all([
+//       UserDb.find({ role: { $ne: "admin" } }),
+//       applicationInfo.find(),
+//       basicInfo.find(),
+//       userFiles.find(),
+//     ]);
+
+//     res.json({ Users, applications, basics, documents });
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// });
+
+
+// module.exports = router;
 const express = require("express");
 const UserDb = require("../../database/models/UserDb");
 const applicationInfo = require("../../database/models/stdDashboard/applicationInfoDb");
@@ -21,9 +60,9 @@ router.get("/", async (req, res) => {
       return res.json({ user }); // Return user object instead of array
     }
 
-    // Fetch all users when no ID is provided
+    // Fetch all users when no ID is provided - SORTED BY CREATION DATE (newest first)
     const [Users, applications, basics, documents] = await Promise.all([
-      UserDb.find({ role: { $ne: "admin" } }),
+      UserDb.find({ role: { $ne: "admin" } }).sort({ createdAt: -1 }), // Sort by createdAt descending (newest first)
       applicationInfo.find(),
       basicInfo.find(),
       userFiles.find(),
@@ -35,6 +74,5 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
-
 
 module.exports = router;
