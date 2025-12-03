da409f3e0d71968a55e2fe72bc6470499650d183
Junaid Khan <juanidsultan92pk@gmail.com>
2025-12-03 16:23:20 +0500

    swagger and airport pick up form

---
+++ swagger.js
@@ -0,0 +1,20 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Your Project API Docs",
+      version: "1.0.0",
+      description: "API documentation for your backend",
+    },
+  },
+
+  // Path of your route files
+  apis: ["./routes/*.js"], 
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = { swaggerUi, swaggerSpec };
